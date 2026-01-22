d8aa5ddfa679ae6e961e53b8ad146e69395d9777
Sabharish Raj G K <rajsabharish@gmail.com>
2026-01-22 15:35:13 +0530

    Add files via upload

---
+++ linked_list-2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+int main() {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+    int n, value;
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        printf("Enter value for node %d: ", i);
+        scanf("%d", &value);
+        newNode->data = value;
+        newNode->next = NULL;
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    printf("Linked List: ");
+    temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL");
+    return 0;
+}
